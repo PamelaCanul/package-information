f436ffc7d6cc1a71fa741601ed0b9ca130e8f233
Pamela Canul <pamelacanul05@gmail.com>
2025-03-24 14:16:04 -0600

    feat(app): Creating the files to configure the app

---
+++ documentos-digitales/src/pages/404.tsx
@@ -0,0 +1,21 @@
+import { NextPage } from "next"
+import Head from "next/head"
+
+const Custom404: NextPage = () => {
+
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-emerald-500 text-white">
+            <Head>
+                <title>¡Página no encontrada!</title>
+            </Head>
+            <div className="flex flex-col justify-center items-center max-w-md p-4">
+                <h1 className="text-7xl md:text-9xl font-bold mb-8">404</h1>
+                <p className="text-md md:text-lg text-center">
+                    ¡Ups! No encontramos la página que buscas
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Custom404
