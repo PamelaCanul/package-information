bf0dff3229e92f84785f95fdb05e72edf5cf2e75
Pamela Canul <pamelacanul05@gmail.com>
2025-03-25 11:01:09 -0600

    improvement(app): Improved app configuration

--- documentos-digitales/next.config.ts
+++ documentos-digitales/next.config.ts
@@ -1,8 +1,67 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const path = require('path');
+// const withNewRelic = require('@newrelic/next');
+
+const nextConfig = {
+  poweredByHeader: false,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
   reactStrictMode: true,
+  images: {
+    domains: ["http://localhost:3000"],
+  },
+  //output: "standalone",
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Content-Type, Accept, Authorization, X-Requested-With, Application",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
+// module.exports = withNewRelic({
+//   newRelicConfig: path.resolve('./newrelic.js'),
+//   ...nextConfig
+// });
